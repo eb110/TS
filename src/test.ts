79edaebe1c3ba48f9379f7b8bd78d61637c2801e
eb110 <wfigura@op.pl>
2024-07-14 21:10:26 +0100

    generic class

--- src/test.ts
+++ src/test.ts
@@ -9,3 +9,10 @@ const logger = new MyLogger();
 logger.log(' joasia');
 
 console.log(logger.dumpLog());
+
+
+const StringDatabase = CreateSimpleMemoryDatabase<string>();
+
+const sdb1 = new StringDatabase();
+//value is automatically set to 'string' as this was declared in line 14
+sdb1.set('a', 'zenek');
